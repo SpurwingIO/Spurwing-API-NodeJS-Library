48bf8b377d9b09778906f0087a211fc06b6efdd1
Ilya Nevolin <justdoitzlol@gmail.com>
2021-03-20 19:41:03 +0100

    bugfixes and cleanup

--- spurwing.js
+++ spurwing.js
@@ -12,22 +12,22 @@ class Spurwing {
     this.API_URL = 'https://api.spurwing.io/api/v2/';
   }
 
-  async get_appointment_types(provider_id, clients_can_book) {
-    return await this.HTTP('GET', this.API_URL + 'appointment_types.json', { provider_id, clients_can_book })
+  async get_appointment_types(provider_id, page_size, offset) {
+    return await this.HTTP('GET', this.API_URL + 'appointment_types.json', { provider_id, page_size, offset })
   }
-  async get_days_available(provider_id, appointment_type_id, date_from_month, timezone, org_level) {
-    return await this.HTTP('GET', this.API_URL + 'bookings/days_available.json', { provider_id, appointment_type_id, date_from_month, timezone, org_level })
+  async get_days_available(provider_id, appointment_type_id, date_from_month, organization_level, timezone) {
+    return await this.HTTP('GET', this.API_URL + 'bookings/days_available.json', { provider_id, appointment_type_id, date_from_month, organization_level, timezone })
   }
-  async get_slots_available(provider_id, appointment_type_id, start_date, end_date, org_level) {
-    return await this.HTTP('GET', this.API_URL + 'bookings/slots_available.json', { provider_id, appointment_type_id, start_date, end_date, org_level })
+  async get_slots_available(provider_id, appointment_type_id, start_date, end_date, organization_level, timezone) {
+    return await this.HTTP('GET', this.API_URL + 'bookings/slots_available.json', { provider_id, appointment_type_id, start_date, end_date, organization_level, timezone })
   }
-  async complete_booking(provider_id, appointment_type_id, date, timezone, first_name, last_name, email, phone_number, contact_type) {
-    return await this.HTTP('POST', this.API_URL + 'bookings/complete_booking.json', { provider_id, appointment_type_id, date, timezone, first_name, last_name, email, phone_number, contact_type })
+  async complete_booking(provider_id, appointment_type_id, email, first_name, last_name, date, contact_type, appointment_id, appointment_location_id, timezone, video_chat_url) {
+    return await this.HTTP('POST', this.API_URL + 'bookings/complete_booking.json', { provider_id, appointment_type_id, email, first_name, last_name, date, contact_type, appointment_id, appointment_location_id, timezone, video_chat_url })
   }
-  async list_appointments(authorization, page_size, offset, provider_id) {
-    return await this.HTTP('GET', this.API_URL + 'appointments', { page_size, offset, provider_id }, { authorization: 'Bearer ' + authorization })
+  async list_appointments(authorization, page_size, offset, appointment_category, load_attendees, load_providers, load_appointment_type) {
+    return await this.HTTP('GET', this.API_URL + 'appointments', { page_size, offset, appointment_category, load_attendees, load_providers, load_appointment_type }, { authorization: 'Bearer ' + authorization })
   }
-  async delete_appointment(appointment_id, authorization) {
+  async delete_appointment(authorization, appointment_id) {
     return await this.HTTP('DELETE', this.API_URL + 'appointments/' + appointment_id, {}, { authorization: 'Bearer ' + authorization })
   }
 
@@ -39,8 +39,11 @@ class Spurwing {
     if (!obj)
       return '';
     let urlData = '';
-    for (let x in obj)
+    for (let x in obj) {
+      if (typeof obj[x] === 'undefined' || obj[x] == null)
+        continue;
       urlData = urlData + x + '=' + encodeURIComponent(obj[x]) + '&';
+    }
     urlData = urlData.substr(0, (urlData.length - 1));
     return urlData;
   }
--- tests.js
+++ tests.js
@@ -14,7 +14,6 @@ const PID = CONFIG.provider_id;
 const KEY = CONFIG.api_key;
 
 const Spurwing = require('./spurwing.js')
-console.log(Spurwing, PID, KEY)
 
 async function runner(testname, func) {
   try {
@@ -36,34 +35,35 @@ function logResp(obj) {
 
   await runner('TEST 1', (async() => {
     let sp = new Spurwing();
-    let tz = "Europe/Brussels";
 
-    let A = await sp.get_appointment_types(PID, true)
+    let A = await sp.get_appointment_types(PID)
     logResp({A})
     A.should.have.lengthOf(3) // default 3
 
-    let B = await sp.get_days_available(PID, A[0].id, dateNow(), tz, false)
+    let appointment_type_id = A[0].id;
+
+    let B = await sp.get_days_available(PID, appointment_type_id)
     logResp({B})
     expect(B.days_available.length).to.be.at.least(1) // at least one day available this month
 
-    let C = await sp.get_slots_available(PID, A[0].id, dateNow(), dateTomorrow(), false)
+    let C = await sp.get_slots_available(PID, appointment_type_id, dateNow(), dateTomorrow())
     logResp({C})
     expect(C.slots_available.length).to.be.at.least(10) // each day has 96 15min slots (60*24 / 15 == 96)
 
     let slot = C.slots_available[5].date
-    let D = await sp.complete_booking(PID, A[0].id, slot, tz, 'Ilya', 'Nevolin', 'ilya2@nevolin.be', '111-111-7777', 'My Contact Type');
+    let D = await sp.complete_booking(PID, appointment_type_id, 'ilya2@nevolin.be', 'Ilya', 'Nevo', slot, 'Test booking');
     logResp({D})
     D.should.have.property('appointment')
     expect(D.appointment.length).to.equal(60)
 
-    let E = await sp.list_appointments(KEY, 1000, 0, PID)
+    let E = await sp.list_appointments(KEY, 1000, 0)
     logResp({E})
     E.should.have.property('data')
     E.data.should.have.property('appointments')
     expect(E.data.appointmentsCount).to.be.at.least(1)
 
     let apid = D.appointment.id;
-    let F = await sp.delete_appointment(apid, KEY)
+    let F = await sp.delete_appointment(KEY, apid)
     logResp({F})
     F.should.have.property('data')
     F.data.should.have.property('appointment')
@@ -73,7 +73,7 @@ function logResp(obj) {
 
   await runner('TEST 2', (async() => {
     let sp = new Spurwing();
-    let A = await sp.list_appointments(KEY, 1000, 0, PID)
+    let A = await sp.list_appointments(KEY, 1000, 0)
     logResp({A})
     A.should.have.property('data')
     A.data.should.have.property('appointments')
